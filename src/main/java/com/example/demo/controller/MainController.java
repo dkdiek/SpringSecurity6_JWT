b6f4abd2ab2bdce85061f7c4562dfd8b0d78a674
Cliff <developerlyk@gmail.com>
2024-04-09 22:53:28 +0900

    SecurityContextHolder 이용 username과 role 값 가져오기

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,15 +1,32 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
 public class MainController {
 
     @GetMapping("/")
-    public String mainP(){
-        return "Main Controller";
+    public String mainP() {
+        /*jwt는 stateless이지만 일시적인 요청에 대해 세션을 잠시 활성화 내부 SecurityContextHolder에서 사용자 정보를 꺼낼 수 있음*/
+
+        // username
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //role
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller" + username + role;
     }
 }
